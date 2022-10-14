025aad717d2ee1d1375c4eeb707bb1d36305b590
Flavio Andrade <foa12381@gmail.com>
2022-10-13 21:46:24 -0300

    feat/src/ structure directorios

---
+++ src/constants/erroMsg.js
@@ -0,0 +1,18 @@
+const userRegister = {
+  userSuccess: 'Usuário foi criado com sucesso',
+  userError: 'Usuário não pode ser criado',
+  userAlreadyExist: 'Usuário já existe na nossa base de dados'
+}
+
+
+
+const userLogin = {
+  userNotFound: 'Usuário não encontrado',
+}
+
+
+module.export = {
+  userRegister,
+  userLogin
+
+}

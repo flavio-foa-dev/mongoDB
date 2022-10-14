8d8c852382f375d69fea0c8f251ad97a4f2d45c9
Flavio Andrade <foa12381@gmail.com>
2022-10-13 21:45:02 -0300

    feat/serve.js/router endpoints

--- serve.js
+++ serve.js
@@ -7,16 +7,156 @@ const jwt = require('jsonwebtoken');
 
 const app = express()
 
+// Config JSON res
+app.use(express.json())
+
+// Models
+const User = require('./src/models/User');
+
 
 // Opn Route - Public Route
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'Roding with Success'})
+app.get('/', async(req, res) => {
+ return res.status(200).json({message: 'Roding with Success'})
+});
+
+// middleware routes
+
+const checkToken = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if(!token) {
+    return res.status(401).json({message: 'Token required'})
+  }
+
+  try {
+    const secretKey = process.env.SECRET_KEY
+    jwt.verify(token, secretKey)
+
+  } catch (error) {
+    console.log(error.message)
+    return res.status(400).json({message: 'Token invalid'})
+  }
+
+  next()
+}
+
+// Private Route - Private Route
+app.get('/user/:id', checkToken, async(req, res) => {
+  const { id } = req.params
+  const user = await User.findById(id, '-password')
+  // delete user.password
+
+
+  if(!user){
+    return res.status(422).json({message: 'User not found'})
+  }
+   return res.status(200).json({user})
+
+})
+
+// login User
+app.post('/auth/login', async(req, res) => {
+  const {email, password} = req.body;
+  if(!email){
+    return res.status(422).json({message: 'Invalid email'})
+  }
+  if(!password){
+    return res.status(422).json({message: 'Invalid password'})
+  }
+
+  // check if the user exists
+  const user = await User.findOne({email:email})
+  if (!user){
+    res.status(422).json({message: 'E-mail not exists'})
+  }
+
+  // check if passwords match
+  const checkPassword = await bcrypt.compare(password, user.password)
+  if (!checkPassword){
+    res.status(422).json({message: 'Passwords invalid'})
+  }
+
+  // tokens
+  try {
+    const secretKey = process.env.SECRET_KEY
+
+    const header ={
+      "alg": "HS256",
+      "typ": "JWT"
+    }
+
+    const payload = {
+      id: user.id,
+      id: user.name,
+      grants: []
+    }
+
+    const config =  {
+      expiresIn: 60 * 60
+    }
+
+    const token = jwt.sign(
+      payload,
+      secretKey,
+      config
+    )
+
+    res.status(200).json({token})
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'service unavailable try later'
+    })
+  }
+
 });
 
 // Register User
 app.post('/auth/register', async(req, res) => {
 
-  const {} = req.body;
+  const {name, email, password, password_confirmation} = req.body;
+
+  // validations
+  if(!name){
+    return res.status(422).json({message: 'Invalid username'})
+  }
+  if(!email){
+    return res.status(422).json({message: 'Invalid email'})
+  }
+  if(!password){
+    return res.status(422).json({message: 'Invalid password'})
+  }
+  if(password !== password_confirmation){
+    return res.status(422).json({message: 'Differnt password'})
+  }
+
+  // Credencials
+  const userExists = await User.findOne({email: email})
+  if(userExists){
+    return res.status(422).json({message: 'E-mail already registered'})
+  }
+
+  // create password  - digitos a mais com o bcrypt
+  const salt = await bcrypt.genSalt(12)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  // Create userRegister
+  const user = new User({
+    name,
+    email,
+    password: passwordHash
+  })
+
+  try {
+  await user.save()
+  res.status(200).json({message: 'User created with Success'})
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+
 
 })
 
